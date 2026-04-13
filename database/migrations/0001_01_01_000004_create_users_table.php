6db0206bfe0477e35141c1183e55b1aa1d262ef1
João Pedro Pratavieira da Silva <joaopedro19042006@gmail.com>
2026-04-13 10:47:44 -0400

    Adicionando Campos picture e document na tabela user

--- database/migrations/0001_01_01_000004_create_users_table.php
+++ database/migrations/0001_01_01_000004_create_users_table.php
@@ -20,6 +20,8 @@ return new class extends Migration
             $table->string('phone');
             $table->string('email');
             $table->boolean('is_counselor');
+            $table->string('picture');
+            $table->string('document');
 
             $table->string('password');
             $table->rememberToken();
