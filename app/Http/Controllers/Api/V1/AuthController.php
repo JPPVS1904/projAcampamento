cfbd7268f87733146a22f3f973278011abfa8c6a
João Pedro Pratavieira da Silva <joaopedro19042006@gmail.com>
2026-04-15 20:25:09 -0400

    substituindo campo email por cpf em loginrequest e authcontroller

--- app/Http/Controllers/Api/V1/AuthController.php
+++ app/Http/Controllers/Api/V1/AuthController.php
@@ -26,11 +26,11 @@ class AuthController extends Controller
 
     public function login(LoginRequest $request): JsonResponse
     {
-        $user = User::where('email', $request->email)->first();
+        $user = User::where('cpf', $request->cpf)->first();
 
         if (! $user || ! Hash::check($request->password, $user->password)) {
             throw ValidationException::withMessages([
-                'email' => ['As credenciais fornecidas estão incorretas.'],
+                'cpf' => ['As credenciais fornecidas estão incorretas.'],
             ]);
         }
 
